0c7d93c6e9560c34d5ef04008cc4e2854442c58c
InbalGam <inbalgam@post.bgu.ac.il>
2023-11-11 16:44:04 +0200

    finished- except google login not connected

--- trip_planner_mobile/Components/Login.js
+++ trip_planner_mobile/Components/Login.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, TextInput, View, Button, Alert, Pressable  } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button, Alert, Pressable, Image, Linking  } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import {login} from '../Api';
 import { Formik } from 'formik';
 import * as Progress from 'react-native-progress';
+import googleIconImageSrc from "../assets/google-icon.png";
+import { baseURL } from '../apiKey';
 
 export default function Login() {
     const [isLoading, setIsLoading] = useState(false);
@@ -22,6 +24,16 @@ export default function Login() {
         return (
           <Pressable style={styles.loginButton} onPress={onPress} disabled={disabled}>
             <Text style={styles.buttonText}>{title}</Text>
+          </Pressable>
+        );
+    };
+
+
+    function GoogleLoginButton(props) {
+        const { onPress } = props;
+        return (
+          <Pressable style={styles.googleButton} onPress={onPress} >
+            <Image source={googleIconImageSrc}/>
           </Pressable>
         );
     };
@@ -71,6 +83,8 @@ export default function Login() {
                             </View>
                         )}
                     </Formik>
+                    <Text style={styles.text}>OR</Text>
+                    <GoogleLoginButton onPress={() => Linking.openURL(`${baseURL}/login/google`)} />
                     <Text style={styles.text} onPress={() => nav.navigate('Signup')}>Dont have an account? Sign Up</Text>
                 </View>}
             {authFailed ? ifAuthFailed() : ''}
@@ -88,7 +102,9 @@ const styles = StyleSheet.create({
     },
     text: {
       fontSize: 16,
-      marginTop: 40
+      marginTop: 50,
+      marginBottom: 40,
+      textAlign: 'center'
     },
     input: {
         borderLeftWidth: 2,
@@ -110,7 +126,11 @@ const styles = StyleSheet.create({
     buttonText: {
         fontSize: 16,
         textAlign: 'center'
-      },
+    },
+    googleButton: {
+        backgroundColor: 'transperant',
+        alignSelf: 'center'
+    },
     link: {}
 });
 
